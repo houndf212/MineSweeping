5ed829fadd52bea6865a3a8c11921384c1b5011f
wangchao <360327741@qq.com>
2016-08-24 11:10:05 +0800

    a return

--- group.cpp
+++ group.cpp
@@ -5,4 +5,5 @@ std::ostream &operator<<(std::ostream &os, const Group &g)
     os<<"all: "<<g.all_unknow.size()<<": "<<g.all_unknow<<std::endl;
     os<<"in : "<<g.innter_border.size()<<": "<<g.innter_border<<std::endl;
     os<<"out: "<<g.outer_border.size()<<": "<<g.outer_border<<std::endl;
+    return os;
 }
